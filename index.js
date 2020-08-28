2d47c18788186aaf7569574663e355ba9b41bcfc
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2020-08-28 21:22:22 +0530

    Add /promotions REST API routes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express'),
       http = require('http'),
       morgan = require('morgan'), // used for logging purposes
       bodyParser = require('body-parser'),
-      dishRouter = require('./routes/dishRouter');
+      dishRouter = require('./routes/dishRouter'),
+      promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -15,6 +16,7 @@ app.use(express.static(__dirname + '/public')); // serving static files
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
 
 const server = http.createServer(app);
 
