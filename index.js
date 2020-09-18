cd243e171ffc7e04f2a0328c5bc50d21d581555f
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2020-09-18 23:48:12 +0530

    Basic Authentication

--- index.js
+++ index.js
@@ -21,7 +21,32 @@ connect.then((db) => {
 
 app.use(morgan('dev'));
 
+function auth(req, res, next) {
+  console.log(req.headers);
+  let authHeader = req.headers.authorization;
+  if (!authHeader) {
+    let err = new Error('You are not authorized');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+    return;
+  }
 
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  let user = auth[0];
+  let pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+
+}
+
+app.use(auth);
 
 app.use(express.static(__dirname + '/public')); // serving static files
 
